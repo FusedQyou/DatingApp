541404dd9b87fa4a6e7e900a6a019aec75a0f411
Roy van der Wijk <FusedQyou@hotmail.com>
2019-12-01 17:04:35 +0100

    Added proper validators

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
@@ -14,10 +15,24 @@ export class RegisterComponent implements OnInit {
 
     model: any = {};
 
+    registerForm: FormGroup;
+
     constructor( private authService: AuthService, private alertify: AlertifyService) {}
-    ngOnInit() {}
+    ngOnInit() {
+        this.registerForm = new FormGroup({
+            username: new FormControl('', Validators.required),
+            password: new FormControl('', [ Validators.required, Validators.minLength(4) ]),
+            confirmPassword: new FormControl('', Validators.required)
+        }, this.passwordMatchValidator);
+    }
+
+    passwordMatchValidator(group: FormGroup) {
+        return group.get('password').value === group.get('confirmPassword').value ? null : {mismatch: true};
+    }
 
     register() {
+        console.log(this.registerForm.value);
+        return;
         this.authService.register(this.model).subscribe(() => {
             this.alertify.success('registration successful.');
         }, error => {
